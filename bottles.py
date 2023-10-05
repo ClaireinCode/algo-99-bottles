27c8adfa02400f297a73a9dbb94feeeb22ad05c8
Claireese <claireese.underwood@gmail.com>
2023-10-05 13:46:14 -0500

    stopped the infinite loop...

--- bottles.py
+++ bottles.py
@@ -1,6 +1,6 @@
 def bottle_song(num):
 	beer = num
-	while beer > 0:
+	while beer > -1:
 		if beer > 1:
 			print(f"{beer} bottles of beer on the wall, {beer} bottles of beer! Take one down, pass it around: {beer - 1} bottles of beer!")
 			beer = beer - 1
@@ -9,6 +9,7 @@ def bottle_song(num):
 			beer = beer - 1
 		else: 
 			print(f"No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, {num} bottles of beer on the wall.")
+			beer = beer - 1
 	pass
 
 bottle_song(88)
